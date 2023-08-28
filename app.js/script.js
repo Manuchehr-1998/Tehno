bc554e8258f575306dcee678715012bf2eaefbee
Manuchehr <manuchehrsaidov998@gmail.com>
2023-08-28 12:56:47 +0500

    Tehno-commit

---
+++ app.js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  $(".slider").slick({
+    arrows: true,
+    dots: true,
+    slidesToShow: 3,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 800,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 550,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 375,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
